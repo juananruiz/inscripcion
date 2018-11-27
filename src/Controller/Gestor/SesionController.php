cbcdf39372ab92ce0732faa517a4bcc19825e59b
Juanan Ruiz <juanan@us.es>
2018-11-27 21:09:21 +0100

    Edición de sesiones de un curso

--- src/Controller/Gestor/SesionController.php
+++ src/Controller/Gestor/SesionController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Gestor;
 use App\Entity\Curso\Sesion;
 use App\Repository\CursoRepository;
 use App\Repository\SesionRepository;
-use App\Repository\SalaRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -23,52 +22,21 @@ class SesionController extends Controller
     protected $repository;
 
 
+    /**
+     * @param SesionRepository $repository
+     */
     function __construct(SesionRepository $repository)
     {
         $this->repository = $repository;
     }
 
     /**
-     * @Route("/listar", name="gestor_sesion_listar")
-     * @Route("/")
-     */
-    public function listar()
-    {
-        $sesions = $this->repository->findAll();
-
-        return $this->render('gestor/sesion/listar.html.twig', [
-            'sesions' => $sesions,
-        ]);
-    }
-
-
-    /**
-     * @Route("/crear", name="gestor_sesion_crear")
-     */
-    public function crear(SalaRepository $salaRepository)
-    {
-        $salas = $salaRepository->findAll();
-        return $this->render('gestor/sesion/crear.html.twig', [
-            'salas' => $salas,
-        ]);
-    }
-
-    /**
-     * @Route("/editar/{id}", requirements={"id": "\d+"}, name="gestor_sesion_editar")
-     */
-    public function editar($id, SalaRepository $salaRepository)
-    {
-        $sesion = $this->repository->find($id);
-        $salas = $salaRepository->findAll();
-        return $this->render('gestor/sesion/editar.html.twig', [
-            'sesion' => $sesion,
-            'salas' => $salas,
-        ]);
-    }
-
-
-    /**
      * @Route("/grabar", name="gestor_sesion_grabar")
+     * @param Request $request
+     * @param CursoRepository $cursoRepository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws ORMException
+     * @throws OptimisticLockException
      */
     public function grabar(Request $request, CursoRepository $cursoRepository)
     {
@@ -76,23 +44,27 @@ class SesionController extends Controller
             $sesion = $this->repository->find($id);
         } else {
             $sesion = new Sesion();
+            // Si es una sesión nueva necesitamos saber el curso
+            if (!$curso_id = $request->get('curso_id')) {
+                return $this->redirectToRoute("gestor_curso_listar");
+            }
+            $curso = $cursoRepository->find($curso_id);
+            $sesion->setCurso($curso);
         }
-        if (!$curso_id = $request->get('curso_id')){
-            return $this->redirectToRoute("gestor_curso_listar");
-        }
-        $curso = $cursoRepository->find($curso_id);
-        $sesion->setCurso($curso)
+        $sesion
             ->setFechaInicio(new \DateTime($request->get('fecha_inicio')))
             ->setDuracion(new \DateTime($request->get('duracion')));
         $this->repository->save($sesion);
 
-        return $this->redirectToRoute("gestor_curso_mostrar", ['id' => $curso_id]);
+        return $this->redirectToRoute("gestor_curso_mostrar", ['id' => $sesion->getCurso()->getId()]);
     }
 
     /**
      * @Route("/borrar/{id}", requirements={"id": "\d+"}, name="gestor_sesion_borrar")
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function borrar($id)
+    public function borrar(int $id)
     {
         $sesion = $this->repository->find($id);
         try {
