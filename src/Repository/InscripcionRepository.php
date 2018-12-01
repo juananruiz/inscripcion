1d0e9e954eb531cb0dbed6ef1acb98f0c84c6de7
Juanan Ruiz <juanan@us.es>
2018-12-01 21:12:35 +0100

    Elimina funciones de ejemplo comentadas

--- src/Repository/InscripcionRepository.php
+++ src/Repository/InscripcionRepository.php
@@ -41,18 +41,4 @@ class InscripcionRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('i.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }
